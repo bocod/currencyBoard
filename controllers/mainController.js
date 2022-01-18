7c51b3f6c51be159cd22a86306f41f11e60c6c30
Agustin Boccio <aboccio@hotmail.com>
2022-01-18 19:28:03 +0100

    Added a home with rates for Argentine Peso, two distinguished views for EURO and Bitcoin rates

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,7 +3,31 @@ const fetch = require( 'node-fetch' );
 module.exports = {
     home: async(req, res) => {
         let allCurrencies;
-        let EURCurrencies;
+        let eurCurrencies;
+        let btcCurrencies;
+
+        await fetch( 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json' )
+            .then( response => response.json() )
+            .then( currenciesList => {
+                allCurrencies = currenciesList;
+            })
+        await fetch( 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur.json' )
+            .then( response => response.json() )
+            .then( currencyVsEUR => { 
+                eurCurrencies = currencyVsEUR;
+            })
+        await fetch( 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/btc.json' )
+            .then( response => response.json() )
+            .then( currencyVsBTC => { 
+                btcCurrencies = currencyVsBTC;
+            })
+
+        res.render( 'index', { allCurrencies, eurCurrencies, btcCurrencies } );
+    },
+    euro: async(req, res) => {
+        let mainCurrency = 'eur';
+        let allCurrencies;
+        let primaryCurrencies;
 
         await fetch( 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json' )
             .then( response => response.json() )
@@ -12,10 +36,28 @@ module.exports = {
             })
         await fetch( 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur.json' )
             .then( response => response.json() )
-            .then( EURvsCurrency => { 
-                EURCurrencies = EURvsCurrency;
+            .then( currencyVsEUR => { 
+                primaryCurrencies = currencyVsEUR;
             })
         
-        res.render( 'index', { allCurrencies, EURCurrencies } ) 
+        res.render( 'rates', { mainCurrency, allCurrencies, primaryCurrencies } ) 
+    },
+    btc: async(req, res) => {
+        let mainCurrency = 'btc';
+        let allCurrencies;
+        let primaryCurrencies;
+
+        await fetch( 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json' )
+            .then( response => response.json() )
+            .then( currenciesList => { 
+                allCurrencies = currenciesList;
+            })
+        await fetch( 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/btc.json' )
+            .then( response => response.json() )
+            .then( currencyVsBTC => { 
+                primaryCurrencies = currencyVsBTC;
+            })
+        
+        res.render( 'rates', { mainCurrency, allCurrencies, primaryCurrencies } ) 
     }
 };
--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -3,5 +3,7 @@ const router =  express.Router();
 const mainController = require( '../controllers/mainController' );
 
 router.get( '/', mainController.home );
+router.get( '/eur', mainController.euro );
+router.get( '/btc', mainController.btc );
 
 module.exports = router;
